74c128bc088eff4e5207c51e3a03b5d31fef2b76
PalmerEldri4 <vadim.karhu@gmail.com>
2021-01-14 12:34:36 -0500

    final S4 (pets)

--- WPFs/Session3.1/WpfSession3.3/WpfApp3/WpfApp3/MainWindow.xaml.cs
+++ WPFs/Session3.1/WpfSession3.3/WpfApp3/WpfApp3/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,21 +16,76 @@ using System.Windows.Shapes;
 
 namespace WpfApp3
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
-        public MainWindow()
-        {
-            InitializeComponent();
-        }
+            public MainWindow()
+            {
+                InitializeComponent();
+            }
 
-        public List<string> people = new List<string>();
+            private void Button_Click(object sender, RoutedEventArgs e)
+            {
+                bool valid = true;
+                string fileName = @"..\..\info.txt";
+                File.AppendAllText(fileName, "Name;Age;pets;" + "\n");
 
-        private void btntest_Click(object sender, RoutedEventArgs e)
-        {
-           
-        }
+                string name = txtName.Text;
+                //you can have validation method to check the name
+                if (name.Length == 0)
+                {
+                    MessageBox.Show("please enter your name", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                    valid = false;
+                }
+                string age = "";
+                if (rbtnBelow18.IsChecked == true)
+                {
+                    age = rbtnBelow18.Content.ToString();
+                }
+                else if (rbtnOver30.IsChecked == true)
+                {
+                    age = rbtnOver30.Content.ToString();
+                }
+
+                else if (rbtnOver80.IsChecked == true)
+                {
+                    age = rbtnOver80.Content.ToString();
+                }
+                else
+                {
+                    MessageBox.Show("please choose one of the age options");
+                    valid = false;
+                }
+                List<string> petList = new List<string>();
+                if (chboxCat.IsChecked == true)
+                {
+                    petList.Add(chboxCat.Content.ToString());
+                }
+                if (chboxDog.IsChecked == true)
+                {
+                    petList.Add(chboxDog.Content.ToString());
+                }
+                if (chboxOther.IsChecked == true)
+                {
+                    petList.Add(chboxCat.Content.ToString());
+                }
+
+                string pets = string.Join(",", petList);
+
+                string continent = cmbContinents.Text;
+                string preferredTemp = lblTemp.VerticalContentAlignment.ToString();
+
+                if (valid == true)
+                {
+                    File.AppendAllText(fileName, $"{name};{age};{pets}");
+                    MessageBox.Show("saved successfully");
+                }
+            }
+
+            private void Window_Loaded(object sender, RoutedEventArgs e)
+            {
+                MessageBox.Show("The form is loaded", "this is my caption",
+                    MessageBoxButton.OKCancel, MessageBoxImage.Exclamation);
+            }
     }
+    
 }
